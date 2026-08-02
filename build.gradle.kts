4832bd273d1f6ce19185434fc1bad886c4c44f9d
baxiy56177 <baxiy56177@amupx.com>
2026-08-02 18:07:02 +0300

    Add project files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.google.devtools.ksp) apply false
+  alias(libs.plugins.roborazzi) apply false
+  alias(libs.plugins.secrets) apply false
+  alias(libs.plugins.google.services) apply false
 }
